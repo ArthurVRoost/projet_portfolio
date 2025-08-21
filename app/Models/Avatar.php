968c735787266412a14373fe00124d8a5d23e38b
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-08-21 10:07:58 +0200

    tables + nav

---
+++ app/Models/Avatar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    /** @use HasFactory<\Database\Factories\AvatarFactory> */
+    use HasFactory;
+    protected $fillable = ['image', 'about_id'];
+
+    public function about(){
+        return $this->belongsTo(About::class);
+    }
+}
---
+++ database/migrations/2025_08_21_072246_create_abouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('subtitle')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('website')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('city')->nullable();
+            $table->unsignedInteger('age')->nullable();
+            $table->string('degree')->nullable();
+            $table->string('email')->nullable();
+            $table->string('freelance')->nullable();
+            $table->string('src')->nullable();
+            $table->text('subtext')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
